c11661a237a881463dc0a0744fa4b16bd82f6cc9
BharatK1 <barath.k1@gmail.com>
2019-05-11 20:06:00 +0200

    Git three

--- Kattis/src/FromPersonal.java
+++ Kattis/src/FromPersonal.java
@@ -4,6 +4,7 @@ public class FromPersonal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I love Satya");
+		System.out.println("Lethargic Satya");
 	}
 
 }
