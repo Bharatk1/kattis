2ffe5a52e1f4e011d6daf67bd565909540ac228a
BharatK1 <barath.k1@gmail.com>
2019-07-14 19:36:02 +0200

    Pig latin new

---
+++ Kattis/src/PL1.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PL1 {
+    public static void main(String args[])throws Exception{
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while(null!=(line=sc.readLine())){
+            String []words = line.split(" ");
+            for(int i=0;i<words.length;i++){
+                String word = words[i];
+                int start=0;
+                for(int j=0;j<word.length();j++){
+                    char let=word.charAt(j);
+                    start=j;
+                    if(let=='a'||let=='e'||let=='i'||let=='o'||let=='u'||let=='y')break;
+                }
+                if(start==0){
+                    System.out.print(word+"yay ");
+                }else{
+                    System.out.print(word.subSequence(start, word.length()) +""+ word.subSequence(0, start)+""+"ay "); 
+                }
+            } System.out.println();
+        }sc.close();
+    }
+}
--- Kattis/src/PigLatin.java
+++ Kattis/src/PigLatin.java
@@ -1,52 +1,44 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class PigLatin {
 	public static void main(String[] args) throws IOException {
-		Scanner scan=new Scanner(System.in);
-		String t=" ";
-		List<String> list=new ArrayList<>();
-		while(t.length()>0) {
-			t=scan.nextLine();
-			if(t.length()>0)
-			list.add(t);
-		}
-		String vowels[]= {"a","e","i","o","u"};
-		List<String> asList = Arrays.asList(vowels);
-		List<String> finalList=new ArrayList<>();
-		list.stream().forEach(sen->{
-			String[] split = sen.split(" ");
-			String fullSentenceAfter="";
-			for(int i=0;i<split.length;i++) {
-				String word=split[i];
+
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+
+		while (null != (line = sc.readLine())) {
+
+			String vowels[] = { "a", "e", "i", "o", "u" };
+			List<String> asList = Arrays.asList(vowels);
+			String[] split = line.split(" ");
+			String fullSentenceAfter = "";
+			for (int i = 0; i < split.length; i++) {
+				String word = split[i];
 				String charAt = Character.toString(word.charAt(0));
-				if(asList.contains(charAt)) {
-					 word=word.concat("yay");
-				}
-				else {
-					String dummy="";
-					String real=word;
-					for(int l=0;l<real.length();l++) {
-						String charac=Character.toString(real.charAt(l));
-						if(asList.contains(charac)){
-							word=word.concat(dummy).concat("ay");
+				if (asList.contains(charAt)) {
+					word = word.concat("yay");
+				} else {
+					String dummy = "";
+					String real = word;
+					for (int l = 0; l < real.length(); l++) {
+						String charac = Character.toString(real.charAt(l));
+						if (asList.contains(charac)) {
+							word = word.concat(dummy).concat("ay");
 							break;
-						}else {
-							word=word.replaceFirst(charac, "");
-							dummy=dummy.concat(charac);
+						} else {
+							word = word.replaceFirst(charac, "");
+							dummy = dummy.concat(charac);
 						}
 					}
 				}
-				fullSentenceAfter=fullSentenceAfter.concat(word+" ");
+				fullSentenceAfter = fullSentenceAfter.concat(word + " ");
 			}
-			finalList.add(fullSentenceAfter);
-		});
-		finalList.stream().forEach(sentence->{
-			System.out.println(sentence.trim());
-		});
-		 scan.close();
+			System.out.println(fullSentenceAfter);
+		}
+		sc.close();
 	}
 }
